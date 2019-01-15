ff95f95221fc22001d0f514a6fb3dcb6f59a4085
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-01-15 18:21:55 +0100

    Make server able to work with parameterized URLs

--- botrytis/db/__init__.py
+++ botrytis/db/__init__.py
@@ -1,15 +1,25 @@
 # coding: utf-8
 
 import sqlite3
+import pkg_resources
 
 from .model import Gene, Annotation, Transcript
 from .generate import generate_db
 
 
-# class Db(object):
-#
-#     def __init__(
-#         self,
-#         path=pkg_resources.resource_filename(__name__, "botrytis.db"),
-#     ):
-#         pass
+class BotrytisDB(object):
+
+    def __init__(self, path=pkg_resources.resource_filename(__name__, "botrytis.db")):
+        self.db = path
+
+    def gene(self, locus):
+        cursor = sqlite3.connect(self.db).cursor()
+        cursor.execute("SELECT * FROM gene WHERE locus=?", (locus,))
+        row = cursor.fetchone()
+        return Gene(*row) if row is not None else None
+
+    def transcript(self, locus):
+        cursor = sqlite3.connect(self.db).cursor()
+        cursor.execute("SELECT * FROM transcript WHERE locus=?", (locus,))
+        row = cursor.fetchone()
+        return Transcript(*row) if row is not None else None
--- botrytis/server/__init__.py
+++ botrytis/server/__init__.py
@@ -3,37 +3,51 @@
 import jinja2
 import cherrypy
 
+from ..db import BotrytisDB
 
-DEFAULT_CONFIGURATION = {
-    "/": {
-        "tools.session.on": True,
-    },
-    '/static': {
-        'tools.staticdir.on': True,
-        'tools.staticdir.dir': './public'
-    }
-}
+
+@cherrypy.popargs("locus")
+class Gene(object):
+
+    def __init__(self, db, env):
+        self.db = db
+        self.env = env
+        self._cp_config = {'error_page.404': self.error404}
+
+    @cherrypy.expose
+    def index(self, locus=None):
+        gene = locus if locus is None else self.db.gene(locus)
+        if gene is None:
+            raise cherrypy.HTTPError(404)
+        template = self.env.get_template('gene.html')
+        return template.render(gene=gene)
+
+    def error404(self, *args, **kwargs):
+        print(args, kwargs)
+        template = self.env.get_template('404.html')
+        return template.render()
+
 
 
 class BotrytisWebsite(object):
 
     def __init__(self):
-        l = jinja2.PackageLoader('botrytis', 'pages')
-        print("TEMPLATES:", l.list_templates())
-        self.env = jinja2.Environment(
+
+        # --- Variables ---
+        self.db = db = BotrytisDB()
+        self.env = env = jinja2.Environment(
             loader=jinja2.PackageLoader('botrytis', 'pages'),
             autoescape=jinja2.select_autoescape(['html', 'xml'])
         )
 
+        # --- Subfolder handlers ---
+        self.gene = Gene(db, env)
+
+
+    def error404(self):
+        return "<html>fuck me, a 404 Error!</html>"
+
+
     @cherrypy.expose
     def index(self):
         return """<html>Hello, world!</html>"""
-
-    @cherrypy.expose
-    def gene(self, id=None):
-        template = self.env.get_template('gene.html')
-        return template.render()
-
-        # if id is None:
-        #     return """<html>Not found.</html>"""
-        # return f"""<html>Requested gene: {id}</html>"""
